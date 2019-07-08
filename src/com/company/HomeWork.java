617036f84754668aa3ef9e48a51aabd903d563b9
SlavcheGj <slavce_gorgiev@hotmail.com>
2019-07-08 14:55:33 +0200

    First Commit

--- src/com/company/HomeWork.java
+++ src/com/company/HomeWork.java
@@ -3,12 +3,14 @@ package com.company;
 public class HomeWork {
     public int numExercises;
     public String exercise;
+    public String subject;
 
-    public HomeWork(int num,String exer){
-        this.numExercises= num;
-        this.exercise=exer;
+    public HomeWork(int num,String exer, String subject){
+        this.subject = subject;
+        this.numExercises = num;
+        this.exercise = exer;
     }
     public void print(){
-        System.out.println("Info"+numExercises+" "+ exercise);
+        System.out.println("You did "+this.numExercises+" form " + this.subject );
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	HomeWork work = new HomeWork(4,"Homework");
+	HomeWork work = new HomeWork(4,"Homework","Math");
 	work.print();
     }
 }
